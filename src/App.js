c28fec0e25d5d5bb1e139ddc1eb747bc093ac23c
yowebraj <mhydary@gmail.com>
2018-12-30 15:45:33 -0500

    added scrollable anchors and custom favicon.ico

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ScrollableAnchor from 'react-scrollable-anchor';
 import './App.css';
 import 'animate.css';
 import TitleImage from './assets/cm-title.png';
@@ -8,37 +9,50 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div className="home-block">
-          <div className="title-container animated infinite tada delay-1s">
-            <img src={TitleImage} alt="" height="300px" width="600px"/>
-          </div>
-
-          <div className="text-container">
-            <p>a podcast by mairaj hydary and friends</p>
-          </div>
-        </div>
-
-        <div className="transition-block-1">
-          <div className="text-container">
-            <p>you're currently in the SECOND section</p>
-          </div>
-        </div>
-
-        <div className="content-block-1">
-          <div className="carousel-container">
+        <ScrollableAnchor id={'p1'}>
+          <div className="home-block">
+            <a href='#p2'>
+              <div className="title-container animated infinite tada delay-1s">
+                <img src={TitleImage} alt="" height="300px" width="600px"/>
+              </div>
+            </a>
+            
             <div className="text-container">
-              <p>SEASON 1</p>
+              a podcast by mairaj hydary and friends
             </div>
+          </div>
+        </ScrollableAnchor>
+
+        <ScrollableAnchor id={'p2'}>
+          <div className="transition-block">
+            <div className="text-container">
+              you're currently in the SECOND section
+            </div>
+
+            <a href='#p3'>
+              <div className="text-container">
+                go to episodes
+              </div>
+            </a>
+          </div>
+        </ScrollableAnchor>
+
+        <ScrollableAnchor id={'p3'}>
+          <div className="content-block">
+            <div className="carousel-container">
+              <div className="text-container">
+                <p>all episodes:</p>
+              </div>
             <AlbumCarousel/>
           </div>
 
-          <div className="carousel-container">
+          <a href="#p1">
             <div className="text-container">
-                <p>SEASON 2</p>
+              go back to the top
             </div>
-            <AlbumCarousel/>
+          </a>
           </div>
-        </div>
+        </ScrollableAnchor>
       </div>
     );
   }
