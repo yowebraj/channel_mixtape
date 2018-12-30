84ee08c9047b967fb870f1674b3445536f87c742
yowebraj <mhydary@gmail.com>
2018-12-30 01:39:42 -0500

    Added title animation and started parallax scrolling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'animate.css';
-import AlbumCarousel from './components/AlbumCarousel'
 import TitleImage from './assets/cm-title.png';
-
+import AlbumCarousel from './components/AlbumCarousel';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div className="home-bg">
-          <div className="title-container">
+        <div className="home-block">
+          <div className="title-container animated infinite tada delay-1s">
             <img src={TitleImage} alt="" height="300px" width="600px"/>
           </div>
 
+          <div className="text-container">
+            <p>a podcast by mairaj hydary and friends</p>
+          </div>
+        </div>
+
+        <div className="transition-block-1">
+          <div className="text-container">
+            <p>you're currently in the SECOND section</p>
+          </div>
+        </div>
+
+        <div className="content-block-1">
           <div className="carousel-container">
+            <div className="text-container">
+              <p>SEASON 1</p>
+            </div>
+            <AlbumCarousel/>
+          </div>
+
+          <div className="carousel-container">
+            <div className="text-container">
+                <p>SEASON 2</p>
+            </div>
             <AlbumCarousel/>
           </div>
         </div>
@@ -24,3 +45,4 @@ class App extends Component {
 }
 
 export default App;
+
